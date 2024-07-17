83085e3e13a9a195244a6599760d31762c25260e
anvy17 <anchovy173@gmail.com>
2024-07-17 16:50:30 +0700

    commit

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-var db = require("../config/db_config");
+const crypto = require("crypto-js");
+require("dotenv").config();
+
+const db = require("../config/db_config");
 
 exports.userLogin = async (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
+  const { username, password } = req.body;
 
   if (!username || !password) {
     return res.status(401).json({
@@ -25,8 +27,6 @@ exports.userLogin = async (req, res) => {
     }
 
     if (result.length > 0) {
-      const user = result[0];
-      console.log(bcrypt.hash(password, 5));
       try {
         const passwordMatch = await bcrypt.compare(password, user.password);
         if (passwordMatch) {
@@ -34,10 +34,16 @@ exports.userLogin = async (req, res) => {
             expiresIn: "2160h",
           });
 
+          const userToken = crypto.AES.encrypt(
+            JSON.stringify({
+              user: user,
+              token: token,
+            }),
+            process.env.JWT_KEY
+          ).toString();
+
           return res.status(200).json({
-            user: user,
-            token: token,
-            message: "Success",
+            userToken: userToken,
           });
         } else {
           return res.status(401).json({
