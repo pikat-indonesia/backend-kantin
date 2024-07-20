f4835382751d5dcecbfca06a1a5c202cf2c8ab95
anvy17 <anchovy173@gmail.com>
2024-07-20 11:58:40 +0700

    test

--- routes/menu.js
+++ routes/menu.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const menuController = require("../controllers/menu");
 const checkAuth = require("../middleware/check-auth");
-const checkAuth = require("../middleware/check-auth");
 
 const router = express.Router();
 router.post("/getMenu", checkAuth, menuController.getMenu);
