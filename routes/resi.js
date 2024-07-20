bb1c9e93b739b227a36c983f4c759f6d568c3a5c
anvy17 <anchovy173@gmail.com>
2024-07-20 12:02:20 +0700

    test

--- routes/resi.js
+++ routes/resi.js
@@ -5,5 +5,8 @@ const checkAuth = require("../middleware/check-auth");
 const router = express.Router();
 router.post("/getResi", checkAuth, resiController.getResi);
 router.post("/addResi", checkAuth, resiController.addResi);
+router.post("/countBarang", checkAuth, resiController.countBarang);
+router.post("/countCost", checkAuth, resiController.countCost);
+router.post("/getPenjualan", checkAuth, resiController.getPenjualan);
 
 module.exports = router;
